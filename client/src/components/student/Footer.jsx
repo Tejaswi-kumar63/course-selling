800d2420bfd35c7539319446c29149335c510336
Tejaswi kumar <brockthebone1389@gmail.com>
2025-08-01 19:37:37 +0530

    final commit

--- client/src/components/student/Footer.jsx
+++ client/src/components/student/Footer.jsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="flex flex-col md:items-start items-center w-full">
           <img src={assets.logo_dark} alt="logo" />
           <p className="mt-6 text-center md:text-left text-sm text-white/80">
-            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text.
+ Classora is your go-to platform for discovering, learning, and mastering new skills through expert-led online courses. Learn at your pace, anytime, anywhere.
           </p>
         </div>
 
